0f61719ce9df50c75e34337b27195a84d7f551a2
Murchey <2371709684@qq.com>
2026-02-18 17:37:30 +0800

    001

---
+++ src/client/java/com/git/mur/efdf/client/EfdfClient.java
@@ -0,0 +1,10 @@
+package com.git.mur.efdf.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EfdfClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/git/mur/efdf/Efdf.java
@@ -0,0 +1,10 @@
+package com.git.mur.efdf;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Efdf implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
